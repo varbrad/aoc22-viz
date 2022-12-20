0f76655466c0203c5e14bb2070ad7c45a5a7ada3
Brad Davies <brad.davies@withwise.com>
2022-12-20 15:56:55 +0000

    Improvements

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,22 +1,37 @@
 import { Canvas } from '@react-three/fiber'
-import { Bounds, Box, OrbitControls, Stage } from '@react-three/drei'
+import {
+  Line,
+  OrbitControls,
+  OrthographicCamera,
+  Stage,
+} from '@react-three/drei'
 import type { LoaderFunction } from '@remix-run/node'
 import { json } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
+import * as THREE from 'three'
 
 import * as d12 from '../days/d12'
 
+const geom = new THREE.BoxGeometry(1, 1, 1)
+
 type LoaderData = {
   nodes: d12.Node[]
-  path: d12.Node[]
+  path: { x: number; y: number; height: number }[]
 }
 
 export const loader: LoaderFunction = () => {
   const p1 = d12.part1()
   let current = p1
-  const path = []
+  const path: LoaderData['path'] = []
   while (current) {
     path.push(current.node)
+    if (current.parent && current.parent.node.height < current.node.height) {
+      path.push({
+        x: current.parent.node.x,
+        y: current.parent.node.y,
+        height: current.node.height,
+      })
+    }
     current = current.parent
   }
   return json<LoaderData>({ nodes: d12.nodes, path })
@@ -28,31 +43,33 @@ export default function Index() {
   return (
     <div className='w-screen min-h-screen h-screen bg-slate-900 p-3'>
       <div className='w-full h-full bg-slate-800'>
-        <Canvas>
-          <Bounds clip observe>
-            <Stage
-              adjustCamera
-              intensity={0.5}
-              shadows='contact'
-              environment='city'
-            >
-              {nodes.map(({ x, y, height }, ix) => (
-                <Box
+        <Canvas frameloop='demand'>
+          <OrthographicCamera makeDefault position={[-10, 8, 8]} />
+          <Stage
+            adjustCamera
+            intensity={0.5}
+            shadows='contact'
+            environment='city'
+          >
+            {nodes.map(({ x, y, height }, ix) => {
+              return (
+                <mesh
                   key={ix}
-                  args={[1, height + 1, 1]}
-                  position={[x, height / 2 + 1, y]}
+                  geometry={geom}
+                  position={[x, height / 2, y]}
+                  scale={[1, height + 1, 1]}
                 >
-                  <meshPhongMaterial color='#f3f3f3' />
-                </Box>
-              ))}
-              {path.map(({ x, y, height }, ix) => (
-                <Box key={ix} args={[1, 1, 1]} position={[x, height + 2, y]}>
-                  <meshPhongMaterial color='magenta' />
-                </Box>
-              ))}
-            </Stage>
-            <OrbitControls makeDefault />
-          </Bounds>
+                  <meshPhongMaterial color='#f3f3f3c' />
+                </mesh>
+              )
+            })}
+            <Line
+              points={path.map(({ x, y, height }) => [x, height + 1, y])}
+              color='hotpink'
+              lineWidth={8}
+            />
+            <OrbitControls />
+          </Stage>
         </Canvas>
       </div>
     </div>
